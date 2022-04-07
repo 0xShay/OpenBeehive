c866ef8e9025c8fd52cd2360b2b3330dc76e4107
Shay <sgaming24@outlook.com>
2022-04-07 10:53:16 +0100

    Allow URI-encoded passwords

--- slogans.js
+++ slogans.js
@@ -4,6 +4,7 @@ const slogans = [
     "Don't need to borrow a laptop anymore to check your assignments!",
     "Say no to infinite loading.",
     "'There was a problem loading the news feed.'",
-    "'<i>undefined</i> is not an object'"
+    "'<i>undefined</i> is not an object'",
+    "Now open-source!"
 ];
 document.getElementById("slogan").innerHTML = slogans[Math.floor(Math.random() * slogans.length)];
