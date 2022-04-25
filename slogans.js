2d429ddc249a8b9f34325e282284ca64c08cfa96
Nerdyboi420 <amish819@gmail.com>
2022-04-25 19:14:09 +0100

    Finished stats
    Updated Slogans.js

--- slogans.js
+++ slogans.js
@@ -1,10 +1,12 @@
 const slogans = [
+    "Restyled by Toast#7529",
     "It's Beehive but better!",
     "No more having to guess your homework from half the title...",
     "Don't need to borrow a laptop anymore to check your assignments!",
     "Say no to infinite loading.",
     "'There was a problem loading the news feed.'",
     "'<i>undefined</i> is not an object'",
-    "Now open-source!"
+    "Now open-source!",
+    "Styled by Amish"
 ];
 document.getElementById("slogan").innerHTML = slogans[Math.floor(Math.random() * slogans.length)];
