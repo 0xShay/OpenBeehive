d08ed7853bcd50370728ee558508dac258323870
Shay <shay@shay.services>
2023-05-02 15:31:57 +0100

    Add "exam season" slogans

--- slogans.js
+++ slogans.js
@@ -1,10 +1,17 @@
-const slogans = [
+
+// [ ORIGINAL SLOGANS ]
+let slogans = [
     "It's Beehive but better!",
-    "No more having to guess your homework from half the title...",
     "Don't need to borrow a laptop anymore to check your assignments!",
-    "Say no to infinite loading.",
-    "'There was a problem loading the news feed.'",
-    "'<i>undefined</i> is not an object'",
     "Now open-source!"
 ];
+
+// [ EXAM SEASON SLOGANS ]
+[
+    "Short-term pain -> long-term gain",
+    "Does someone smell burning toast?"
+].forEach(s => {
+    slogans.append(s);
+});
+
 document.getElementById("slogan").innerHTML = slogans[Math.floor(Math.random() * slogans.length)];
